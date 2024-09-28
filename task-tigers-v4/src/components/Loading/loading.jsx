44a39c95db3946115e1c68b595912ac2e4663b9f
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-09-28 12:47:04 +0530

    added the Services and maincategory

--- task-tigers-v4/src/components/Loading/loading.jsx
+++ task-tigers-v4/src/components/Loading/loading.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import logo from '../../assets/images/flash.png'
-import './loading.css'
+// File: src/components/Loading/loading.js
 
-const loading = () => {
+import React from "react";
+import tigerSplashscreen from "../../assets/images/tiger-splashscreen.gif"; // Import the GIF
+import "./loading.css";
+
+const Loading = () => {
   return (
     <>
-    <div className='loading-main-con'>
-        <div className='loading-sub-con'> 
-            <img src={logo} alt='animation'/>
+      <div className="loading-main-con">
+        <div className="loading-sub-con">
+          {/* Display the GIF */}
+          <img
+            src={tigerSplashscreen}
+            alt="splashscreen animation"
+            width="100%"
+            height="100%"
+          />
         </div>
-        <div className='loadrtl'>
-
-        </div>
-    </div>
-      
+        <div className="loadrtl">{/* Additional content if needed */}</div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default loading
+export default Loading;
